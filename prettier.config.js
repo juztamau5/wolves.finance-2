33e70339cf96da5ecb83377d795beca7c26eb01d
juztamau5 <juztamau5@gmail.com>
2020-12-01 14:53:45 -0800

    Add Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 wolves.finance developers
+ * This file is part of wolves.finance - https://github.com/peak3d/wolves.finance
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+module.exports = {
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
