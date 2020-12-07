47c2df184d735504828cc5a6bb18a00fdbac0093
juztamau5 <juztamau5@gmail.com>
2020-12-07 15:53:10 -0800

    Add license headers to smart contracts

--- contracts/src/investment/StakeFarm.sol
+++ contracts/src/investment/StakeFarm.sol
@@ -1,4 +1,11 @@
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright (C) 2020 wolves.finance developers
+ * This file is part of wolves.finance - https://github.com/peak3d/wolves.finance
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See the file LICENSES/README.md for more information.
+ */
+
 pragma solidity >=0.6.0 <0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
