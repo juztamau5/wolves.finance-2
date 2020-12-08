23d77f3720514ff208a96580295e578cc8b1ed2e
juztamau5 <juztamau5@gmail.com>
2020-12-08 10:47:16 -0800

    Contracts: Replace Ownable code with OZ import

--- contracts/src/investment/controller.sol
+++ contracts/src/investment/controller.sol
@@ -8,67 +8,8 @@
 
 pragma solidity 0.6.5;
 
+import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
-
-contract Context {
-  constructor() internal {}
-
-  // solhint-disable-previous-line no-empty-blocks
-
-  function _msgSender() internal view returns (address payable) {
-    return msg.sender;
-  }
-
-  function _msgData() internal view returns (bytes memory) {
-    // silence state mutability warning without generating bytecode - see
-    // https://github.com/ethereum/solidity/issues/2691
-    this;
-
-    return msg.data;
-  }
-}
-
-contract Ownable is Context {
-  address payable private _owner;
-
-  event OwnershipTransferred(
-    address indexed previousOwner,
-    address indexed newOwner
-  );
-
-  constructor() internal {
-    _owner = _msgSender();
-    emit OwnershipTransferred(address(0), _owner);
-  }
-
-  function owner() public view returns (address payable) {
-    return _owner;
-  }
-
-  modifier onlyOwner() {
-    require(isOwner(), 'Ownable: caller is not the owner');
-    _;
-  }
-
-  function isOwner() public view returns (bool) {
-    return _msgSender() == _owner;
-  }
-
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
-  }
-
-  function transferOwnership(address payable newOwner) public onlyOwner {
-    _transferOwnership(newOwner);
-  }
-
-  function _transferOwnership(address payable newOwner) internal {
-    require(newOwner != address(0), 'Ownable: new owner is the zero address');
-    emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
-  }
-}
 
 interface IFarm {
   function name() external view returns (string memory);
